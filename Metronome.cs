831f40a4c5fc9b4a11170d421585ce14108549bf
Nodgez <nodgez@hotmail.com>
2014-09-09 23:17:32 +0100

    Simple Metronome for Unity

--- Metronome.cs
+++ Metronome.cs
@@ -14,32 +14,35 @@ public class Metronome : MonoBehaviour {
 	private float alpha;
 	private float interval;
 
-	void Start ()
+	void Start()
 	{
-		interval = 60.0f / (float)tempo;
-		Time.fixedDeltaTime = interval;
+		audio.loop = false;
 	}
 
 	void Update()
 	{
+		//check to see if note is an approprate value e.g. whole,2nd,4th,8th,16th,32th notes
 		if(!IsPowerOfTwo(noteValue))
 			noteValue = 4;
 
+		//set the tempo to act according to the not value
 		float scaling = (float)4 / (float)noteValue;
 
+		//set the fixedDeltaTime that plays our metronome in the FixedUpdate
 		interval = (60.0f * scaling) / (float)tempo;
 		Time.fixedDeltaTime = interval;
 	}
 
 	void FixedUpdate () 
 	{
+		//accent our beat on the 1st hit of every bar
 		audio.clip = beatCounter % beatsInBar == 0 ? accent : normal;
 		beatCounter++;
-
-		audio.loop = false;
+	
 		audio.Play ();
 	}
 
+	//simple metod to check if our value is a power of 2
 	bool IsPowerOfTwo(int value)
 	{
 		return (value != 0) && ((value & (value - 1)) == 0);
